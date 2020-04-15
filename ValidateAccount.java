4b861eb89738734770e777d3aae7eae3ca7e9813
badat <you@example.com>
2020-04-15 15:14:07 +0700

    validateAccount

---
+++ ValidateAccount.java
@@ -0,0 +1,21 @@
+package ValidateAccount;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateAccount {
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String ACCOUNT_REGEX = "^[_a-z0-9]{6,}$";;
+
+    public ValidateAccount() {
+
+    }
+
+    public boolean validate(String regex) {
+        pattern = Pattern.compile(ACCOUNT_REGEX);
+        matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
